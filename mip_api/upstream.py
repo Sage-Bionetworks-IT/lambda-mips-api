93d6e4e21b81c976a75ada5eb8ad8b44f51e0eda
Joni Harker <joni.harker@sagebase.org>
2025-07-18 12:27:13 -0700

    [IT-4485] Add endpoint to show GL account balances
    Add a new API endpoint `/balances` to output a CSV of GL account
    balances appropriate for sending to FloQast.
    
    During the first week of the month, process the balance for the previous
    month, otherwise process the month-to-date balance.
    
    The pre-existing endpoints used the Program segment of the chart of
    accounts, but the new endpoint uses the GL segment of the chart of
    accounts. Refactor fetching the chart of accounts to support arbitrary
    segments.

--- mip_api/upstream.py
+++ mip_api/upstream.py
@@ -1,5 +1,7 @@
 import logging
 
+from mip_api import util
+
 import backoff
 import requests
 from requests.exceptions import RequestException
@@ -12,6 +14,9 @@ mip_url_login = "https://login.mip.com/api/v1/sso/mipadv/login"
 mip_url_coa_segments = "https://api.mip.com/api/coa/segments"
 mip_url_coa_accounts = "https://api.mip.com/api/coa/segments/accounts"
 mip_url_logout = "https://api.mip.com/api/security/logout"
+mip_url_current_balance = (
+    "https://api.mip.com/api/model/CBODispBal/methods/GetAccountBalances"
+)
 
 
 @backoff.on_exception(backoff.expo, (RequestError, RequestException), max_time=11)
@@ -57,7 +62,7 @@ def _logout(access_token):
 
 
 @backoff.on_exception(backoff.expo, (RequestError, RequestException), max_time=11)
-def _get_program_segment(access_token):
+def _get_segment_id(access_token, segment_name):
     """
     Wrap the request for chart segment IDs with backoff decorator, using
     exponential backoff and running for at most 11 seconds. With a
@@ -80,18 +85,18 @@ def _get_program_segment(access_token):
     # get the segment ID for the "Program" segment
     seg_id = None
     for segment in json_response["COA_SEGID"]:
-        if segment["TITLE"] == "Program":
+        if segment["TITLE"] == segment_name:
             seg_id = segment["COA_SEGID"]
             break
 
     if seg_id is None:
-        raise ValueError("Program segment not found")
+        raise ValueError(f"Segment {segment_name} not found")
 
     return seg_id
 
 
 @backoff.on_exception(backoff.expo, (RequestError, RequestException), max_time=11)
-def _get_chart(access_token, seg_id):
+def _get_chart_segment(access_token, seg_id, hide_inactive):
     """
     Wrap the request for chart of accounts with backoff decorator, using
     exponential backoff and running for at most 11 seconds. With a
@@ -113,15 +118,66 @@ def _get_chart(access_token, seg_id):
 
     accounts = {}
     for account in json_response["COA_SEGID"]:
-        # require "Program" segment and "A" status
-        if account["COA_SEGID"] == seg_id and account["COA_STATUS"] == "A":
+        if account["COA_SEGID"] == seg_id:
+            if hide_inactive and account["COA_STATUS"] != "A":
+                LOG.info(f"Hiding inactive account: {account['COA_TITLE']}")
+                continue
             accounts[account["COA_CODE"]] = account["COA_TITLE"]
 
     LOG.info(f"Chart of accounts: {accounts}")
     return accounts
 
 
-def program_chart(org_name, secrets):
+@backoff.on_exception(backoff.expo, (RequestError, RequestException), max_time=11)
+def _get_balance(access_token, period_from, period_to):
+    timeout = 4
+    LOG.info("Getting balances")
+
+    # copied from chrome dev tools while clicking through the web ui
+    body = {
+        "BOInformation": {
+            "constructor": {},
+            "ModelFields": {
+                "fields": [
+                    {"DISPBAL_DATEFROM": period_from},
+                    {"DISPBAL_DATETO": period_to},
+                ],
+                "DISPBAL_SEGINFO": [
+                    {
+                        "fields": [
+                            {"GRID_PHY_ID": 0},
+                            {"FILTER_SELECT": True},
+                            {"FILTER_ORDER": -1},
+                            {"FILTER_FIX": False},
+                            {"FILTER_DATATYPE": 10},
+                            {"FILTER_FIELDTYPE": 40},
+                            {"FILTER_ITEM": "GL"},
+                            {"FILTER_OPERATOR": "<>"},
+                            {"FILTER_CRITERIA1": "<Blank>"},
+                            {"FILTER_CRITERIA2": ""},
+                        ]
+                    }
+                ],
+            },
+        },
+        "MethodParameters": {"strJson": '{"level":1}'},
+    }
+
+    api_response = requests.post(
+        mip_url_current_balance,
+        headers={"Authorization-Token": access_token},
+        timeout=timeout,
+        json=body,
+    )
+    api_response.raise_for_status()
+    json_response = api_response.json()
+    LOG.debug(f"Raw balance documents json: {json_response}")
+
+    balance = json_response
+    return balance
+
+
+def get_chart(org_name, secrets, segment, hide_inactive):
     """
     Log into MIPS, get the chart of accounts, and log out
     """
@@ -140,10 +196,10 @@ def program_chart(org_name, secrets):
         access_token = _login(mip_creds)
 
         # get the program segment id
-        program_id = _get_program_segment(access_token)
+        segment_id = _get_segment_id(access_token, segment)
 
         # get the chart of program accounts
-        chart_dict = _get_chart(access_token, program_id)
+        chart_dict = _get_chart_segment(access_token, segment_id, hide_inactive)
 
     except Exception as exc:
         LOG.exception("Error interacting with upstream API")
@@ -158,3 +214,37 @@ def program_chart(org_name, secrets):
                 LOG.exception("Error logging out")
 
     return chart_dict
+
+
+def trial_balances(org_name, secrets, when=None):
+    bal_dict = {}
+    access_token = None
+
+    start_str, end_str = util.target_period(when)
+
+    mip_creds = {
+        "username": secrets["user"],
+        "password": secrets["pass"],
+        "org": org_name,
+    }
+
+    try:
+        # get mip access token
+        access_token = _login(mip_creds)
+        bal_dict = _get_balance(access_token, start_str, end_str)
+    except Exception as exc:
+        LOG.exception(exc)
+    finally:
+        # It's important to logout. Logging in a second time without
+        # logging out will lock us out of the upstream API
+        try:
+            _logout(access_token)
+        except Exception as exc:
+            LOG.exception("Error logging out")
+
+    bal_dict["period_from"] = start_str
+    bal_dict["period_to"] = end_str
+
+    LOG.debug(f"Balance dict: {bal_dict}")
+
+    return bal_dict
