49bb9b35776bfe224ba2955ed96ce45a94627cfa
Joni Harker <joni.harker@sagebase.org>
2025-07-25 10:52:13 -0700

    Review feedback

--- mip_api/util.py
+++ mip_api/util.py
@@ -1,6 +1,16 @@
 import os
 
 
+def dict_prepend(original, key, value):
+    # Since Python 3.7, python dictionaries preserve insertion
+    # order, so to prepend an item to the top of the dictionary,
+    # we create a new dictionary inserting the new key first,
+    # then add the previous output
+    new_dict = {key: value}
+    new_dict.update(original)
+    return new_dict
+
+
 def get_os_var(varnam):
     try:
         return os.environ[varnam]
@@ -22,6 +32,17 @@ def _param_bool(params, param):
     return False
 
 
+def _param_int(params, param):
+    number = 0
+    if params and param in params:
+        try:
+            number = int(params[param])
+        except ValueError as exc:
+            msg = f"The parameter '{param}' must be an integer"
+            raise ValueError(msg) from exc
+    return number
+
+
 def param_inactive_bool(params):
     return not _param_bool(params, "show_inactive_codes")
 
@@ -35,13 +56,10 @@ def param_no_program_bool(params):
 
 
 def param_limit_int(params):
-    if params and "limit" in params:
-        try:
-            return int(params["limit"])
-        except ValueError as exc:
-            err_str = "QueryStringParameter 'limit' must be an Integer"
-            raise ValueError(err_str) from exc
-    return 0
+    limit = _param_int(params, "limit")
+    if limit < 0:
+        raise ValueError("The parameter 'limit' must be a positive Integer")
+    return limit
 
 
 def param_priority_list(params):
